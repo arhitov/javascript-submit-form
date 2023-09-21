f2c18861ed39cdac13c14e3469655d3c65cc00da
clgsru <clgsru@gmail.com>
2023-09-21 20:37:09 +0300

    Main version

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
-# javascript-submit-form
 Form Submit
+==============
+
+[![bootstrap][ico-bootstrap-support]][link-bootstrap-support]
+[![Software License][ico-license]][link-license]
+
+This is JavaScript for sending form data using ajax. The script displays a response message and can perform a redirect.
+
+## Supported
+* [x] Send form data
+* [x] Show return message
+* [x] Redirect
+
+## Requirements
+* bootstrap (5.0+)
+
+## Sponsor my work!
+If you think this package helped you in any way, you can sponsor me! I am a free developer, so your help will be very helpful to me. :blush:
+
+## License
+
+The script is open-sourced software licensed under the [MIT license][link-license].
+
+## Authors
+Alexander Arhitov [clgsru@gmail.com](mailto:clgsru@gmail.com)
+
+Welcome here! :metal:
+
+[ico-bootstrap-support]: https://img.shields.io/badge/bootstrap-5.x-712cf9.svg
+[link-bootstrap-support]: https://laravel.com/docs/8.x/
+[ico-license]: https://img.shields.io/badge/license-MIT-brightgreen.svg
+[link-license]: LICENSE.md
+[link-example]: Example
---
+++ src/script.js
@@ -0,0 +1,199 @@
+/**
+* Form Submit - v0.1
+* URL: https://github.com/arhitov/javascript-submit-form
+* Author: Alexander Arhitov clgsru@gmail.com
+*/
+(function () {
+    "use strict";
+
+    // form.submit-form
+    //      [data-reset_form="false"]
+    // .submit-form-message-error
+    // .submit-form-message-success
+    // .submit-form-button-submit
+    // .submit-form-loading
+
+    function toggle (parentNode, selector) {
+        const element = (selector instanceof Element)
+            ? selector
+            : parentNode.querySelector(selector);
+        return {
+            show: element
+                ? function (str = null) {
+                    element.classList.remove('d-none');
+                    if (str) {
+                        element.innerHTML = str;
+                    }
+                }
+                : (str) => null,
+            hide: element
+                ? function () {
+                    element.classList.add('d-none');
+                }
+                : () => null,
+        };
+    }
+
+    function initForm (selfForm) {
+        let thisForm = selfForm;
+        let dataForm= new FormData( thisForm );
+        const loading= toggle(thisForm, '.submit-form-loading');
+        const message_error = toggle(thisForm, '.submit-form-message-error');
+        const message_success= toggle(thisForm, '.submit-form-message-success');
+        const button_submit = (function () {
+            const element = thisForm.querySelector('.submit-form-button-submit');
+            return {
+                lock: element
+                    ? function () {
+                        element.setAttribute('disabled', 'disabled');
+                    }
+                    : (str) => null,
+                unlock: element
+                    ? function () {
+                        element.removeAttribute('disabled');
+                    }
+                    : () => null,
+            };
+        })();
+        const displayLoading = function () {
+            button_submit.lock();
+            loading.show();
+            message_error.hide();
+            message_success.hide();
+        };
+        const displaySuccess = function (success = '') {
+            button_submit.unlock();
+            loading.hide();
+            message_success.show(success);
+
+            if ('' === (thisForm.getAttribute('data-reset_form') ?? '')) {
+                thisForm.reset();
+            }
+        };
+        const displayError = function (error) {
+            button_submit.unlock();
+            loading.hide();
+            message_error.show(error);
+        };
+
+        return {
+            form: thisForm,
+            data: dataForm,
+            displayLoading: displayLoading,
+            displaySuccess: displaySuccess,
+            displayError: displayError,
+            submit: function () {
+                let action = thisForm.getAttribute('action');
+                fetch(action, {
+                    method: 'POST',
+                    body: dataForm,
+                    headers: {
+                        'Accept': 'application/json',
+                        'X-Requested-With': 'XMLHttpRequest'
+                    }
+                })
+                .then(async response => {
+                    try {
+                        return {
+                            response: response,
+                            answer: await response.json()
+                        };
+                    } catch (e) {
+                        console.error(e);
+                        throw new Error(`${response.status} ${response.statusText} ${response.url}`);
+                    }
+                })
+                .then(data => {
+                    if (data.response.ok) {
+                        return {
+                            status: data.response.status,
+                            answer: data.answer
+                        };
+                    } else if (data.answer.message) {
+                        throw new Error(`${data.answer.message}`);
+                    } else {
+                        throw new Error(`${data.response.status} ${data.response.statusText} ${data.response.url}`);
+                    }
+                })
+                .then(data => {
+                    if (data.answer.message) {
+                        displaySuccess(data.answer.message);
+                        return data.answer;
+                    } else if (201 === data.status) {
+                        displaySuccess('Successful');
+                        return data.answer;
+                    } else if (202 === data.status) {
+                        displaySuccess('Successful');
+                        return data.answer;
+                    } else {
+                        throw new Error(data ? data : 'Form submission failed and no error message returned from: ' + action);
+                    }
+                })
+                .then(answer => {
+                    if (answer.redirect_to) {
+                        sleep(2000).then(() => {
+                            window.location.href = answer.redirect_to;
+                        });
+                    }
+
+                    const event = thisForm.getAttribute('data-event-success');
+                    if (event) {
+                        document.dispatchEvent(new CustomEvent('submit-form.' + event, {
+                            detail: {
+                                form: thisForm,
+                                data: dataForm,
+                                answer: answer
+                            }
+                        }));
+                    }
+                })
+                .catch((error) => {
+                    console.error(error);
+                    displayError(error);
+                });
+            }
+        };
+    }
+
+    ([].slice.call(document.querySelectorAll('form.submit-form'))).forEach(element => {
+        element.addEventListener('submit', event => {
+            event.preventDefault();
+
+            const form = initForm(element);
+            let action = form.form.getAttribute('action');
+            let recaptcha = form.form.getAttribute('data-recaptcha-site-key');
+
+            if( ! action ) {
+                form.displayError('The form action property is not set!');
+                return;
+            }
+
+            form.displayLoading();
+
+            if ( recaptcha ) {
+                if(typeof grecaptcha !== "undefined" ) {
+                    grecaptcha.ready(function() {
+                        try {
+                            grecaptcha.execute(recaptcha, {action: 'submit'})
+                            .then(token => {
+                                form.data.set('recaptcha-response', token);
+                                form.submit();
+                            })
+                        } catch(error) {
+                            form.displayError(error);
+                        }
+                    });
+                } else {
+                    form.displayError('The reCaptcha javascript API url is not loaded!')
+                }
+            } else {
+                form.submit();
+            }
+        });
+    });
+
+    function sleep(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+    }
+
+})();
